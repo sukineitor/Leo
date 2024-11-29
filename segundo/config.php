7876c41e3148c7004e73ffd3dca5e1c9894d9c1f
sukineitor <130964222+sukineitor@users.noreply.github.com>
2024-11-29 18:19:49 -0500

    Update config.php

--- segundo/config.php
+++ segundo/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.railway.internal');
 define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'link_sharing_site');
+define('DB_PASS', 'yjZDmqlEUbJphvJAXivwhPhLCWgsnMHC');
+define('DB_NAME', 'railway');
 ?>
