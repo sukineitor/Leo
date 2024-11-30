d8ced4ea4e14e7bc14834f4c78b0b7b4d187a7f0
sukineitor <130964222+sukineitor@users.noreply.github.com>
2024-11-30 09:50:19 -0500

    Update config.php

--- segundo/config.php
+++ segundo/config.php
@@ -1,6 +1,7 @@
 <?php
-define('DB_HOST', 'mysql.railway.internal');
-define('DB_USER', 'root');
-define('DB_PASS', 'yjZDmqlEUbJphvJAXivwhPhLCWgsnMHC');
-define('DB_NAME', 'railway');
+define('DB_HOST', 'ep-sparkling-firefly-a591u40r.us-east-2.aws.neon.tech'); // Host de Neon
+define('DB_USER', 'link_sharing_site_owner'); // Usuario
+define('DB_PASS', 'LD7gFGSr5izV'); // Contraseña
+define('DB_NAME', 'link_sharing_site'); // Base de datos
+define('DB_PORT', '5432'); // Puerto estándar de PostgreSQL
 ?>
