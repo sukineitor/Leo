1e696023eb470691be0618c15b35de31e2ec628c
sukineitor <130964222+sukineitor@users.noreply.github.com>
2024-12-01 16:59:02 -0500

    Update db_connect.php

--- segundo/db_connect.php
+++ segundo/db_connect.php
@@ -1,10 +1,11 @@
 <?php
 require_once 'config.php';
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// Establecer la conexión con PostgreSQL
+$conn = pg_connect("host=" . DB_HOST . " dbname=" . DB_NAME . " user=" . DB_USER . " password=" . DB_PASS);
 
-if ($conn->connect_error) {
-    error_log("Connection failed: " . $conn->connect_error);  // Esto te ayuda a ver el error en el log de errores.
+if (!$conn) {
+    error_log("Connection failed: " . pg_last_error());  // Esto te ayudará a ver el error en el log de errores.
     die("Error connecting to the database. Please try again later.");
 }
 ?>
