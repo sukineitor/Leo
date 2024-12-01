661e7933cd0191392b6f5db275845b3d00293c9b
sukineitor <130964222+sukineitor@users.noreply.github.com>
2024-12-01 16:58:51 -0500

    Update config.php

--- segundo/config.php
+++ segundo/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'link_sharing_site');
+define('DB_HOST', 'dpg-ct6dlshu0jms7395n3l0-a');  // Tu hostname de Render
+define('DB_USER', 'link_sharing_site_user');      // Tu nombre de usuario de la base de datos
+define('DB_PASS', '7VBdOjs7sL8fgXh4WqPBmDGW4v7i7LK6');  // Tu contraseña de la base de datos
+define('DB_NAME', 'link_sharing_site');           // El nombre de la base de datos
 ?>
